30b8729adf2773a6213d78694e3ad67a2c844ea9
tfihsnz <ljburrough@gmail.com>
2018-07-31 01:38:03 +1200

    added the finished files from the 2nd week workshop

--- run.py
+++ run.py
@@ -1,11 +1,38 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# to run:
+# cd to directory
+# source env/bin/activate
+# export FLASK_APP=run.py; export FLASK_DEBUG=1
+# flask run
+
+burgers = [
+ ['Classic Burger', '$4.99'],
+ ['Cheese Burger', '$5.99'],
+ ['Chicken Burger', '$5.99'],
+ ['Double Burger', '$6.99']
+]
+
+drinks = [
+ ['Cola', '$0.99'],
+ ['Ginger Ale', '$0.99'],
+ ['Beer', '$1.99'],
+ ['Coffee', '$1.99']
+]
+
+sides = [
+ ['Fries', '$1.99'],
+ ['Onion Rings', '$1.99'],
+ ['Mushrooms', '$1.99'],
+ ['Salad', '$1.99']
+]
+
 @app.route('/')
 def index():
-    return('<h1>212 Borger</h1>')
+  return render_template('index.html', disclaimer='may contain traces of nuts', burgers=burgers, drinks=drinks, sides=sides)
 
-@app.route('/contact')
-def contact():
-    return('meboi@gmail.com')
+@app.route('/order')
+def order():
+  return render_template('order.html')
